b8a4e8bf32e3847c186e8475edb7e26fa65ea836
OmarAbdelhamidAly <omar.yaser.o.1322001@gmail.com>
2024-12-14 07:20:39 +0200

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -75,9 +75,7 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, use_reloader=False)
-    
-    
+    app.run(debug=True, host='0.0.0.0', port=5000)
     
     
     
